c26cd50b38376cfd81385f48f958ee87230a9160
maccha <60777705+macchanism@users.noreply.github.com>
2022-09-10 16:39:46 +0900

    update template.py

---
+++ template.py
@@ -0,0 +1,17 @@
+import bisect
+import collections as cl # deque,Counter,defaultdict
+import itertools as it # accumulate,permutations,combinations
+import heapq # heapify,heappush,heappop
+import math # gcd,factorial,ceil
+import sys
+sys.setrecursionlimit(10**9)
+#input = sys.stdin.readline
+
+def IP_S(): return input()
+def IP_I(): return int(input())
+def IP_MS(): return map(str, input().split())
+def IP_MI(): return map(int, input().split())
+def IP_LS(): return list(map(str, input().split()))
+def IP_LI(): return list(map(int, input().split()))
+
+##### Source code starts from here #####
